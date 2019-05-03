bee840076d329812a897e31814b80b77075949b2
DarcyNguyen <chuyen.nn@icloud.com>
2019-05-03 17:35:03 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
+var Schema = mongoose.Schema
+mongoose.connect("mongodb+srv://anonymous:oGDF9HIRt3PMxGN0@cluster0-ioiff.mongodb.net/test?retryWrites=true"
+    ,{useNewUrlParser:true});
 
 app.set("view engine","ejs");
 
@@ -10,4 +12,28 @@ app.get("/", function(req,res){
     res.render("hello")
 })
 
-app.listen(process.env.PORT, process.env.IP)
+var studentSchema = new Schema({
+    name: String,
+    age: String
+})
+
+var Student = mongoose.model("Student", studentSchema)
+
+var chuyen = new Student({
+    name:"Nguyen Ngoc Chuyen",
+    age: "3"
+})
+
+Student.create(chuyen, function(newStudent, error){
+    if (error){
+        console.log(error)
+    } else {
+        console.log(newStudent)
+    }
+})
+
+
+
+app.listen(3000,function(){
+    console.log("running")
+})
