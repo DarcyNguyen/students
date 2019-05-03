1e39cb8126f345947563ac35f8b5681b3881913b
DarcyNguyen <chuyen.nn@icloud.com>
2019-05-03 16:05:59 +0700

    a

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
 
-app.use("view engine","ejs");
+app.set("view engine","ejs");
+
 
 app.get("/", function(req,res){
     res.render("hello")
