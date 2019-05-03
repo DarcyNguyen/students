71df8408791f43baacd0d9f149deab5826a15c61
DarcyNguyen <chuyen.nn@icloud.com>
2019-05-03 16:01:56 +0700

    ahha

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use("view engine","ejs");
 
 app.get("/", function(req,res){
-    res.render("Hello")
+    res.render("hello")
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
